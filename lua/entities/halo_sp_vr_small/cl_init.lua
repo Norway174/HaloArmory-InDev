0650d49ab9ddcc1ba66fbbcfb7f6b7ae89964d73
Norway174 <norway174@terrancom.net>
2024-01-19 16:35:18 +0100

    Add wireframe sphere to halo_sp_vr_small entity for debugging

--- lua/entities/halo_sp_vr_small/cl_init.lua
+++ lua/entities/halo_sp_vr_small/cl_init.lua
@@ -3,4 +3,9 @@ include('shared.lua')
 
 function ENT:DrawTranslucent()
     self:DrawModel()
+
+
+    --local pos = self:GetPos() + self.VehicleSpawnPos
+
+    --render.DrawWireframeSphere( pos, 100, 10, 10, Color( 9, 177, 255), true)
 end
